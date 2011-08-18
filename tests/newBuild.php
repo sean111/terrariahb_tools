82bc802961529669e848b7b60a8215216b340707
Sean Burke <seanb@eshark.net>
2011-08-18 12:31:34 -0400

    Updates for the test parser

--- tests/newBuild.php
+++ tests/newBuild.php
@@ -1,9 +1,40 @@
 <?php
-require_once 'config.php';
+require_once '../config.php';
 require_once 'phpQuery.php';
-$templateFile='/srv/www/droid/thb/template.html';
-$outputFile='/srv/www/droid/thb/index.html';
-//Query: SELECT m.id, m.name, m.link as mlink, o.name, o.img, o.link as olink, c.id as cid, c.name FROM menu m LEFT OUTER JOIN objects o ON m.id=o.mid LEFT OUTER JOIN cats c ON o.cid=c.id;
+$templateFile='template.html';
+$outputFile='index.html';   
 
+$db=new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+$sql=$db->query("SELECT m.id, m.name as name, m.link as mlink, o.name as oname, o.img, o.link as olink, c.id as cid, c.name as cname FROM menu m LEFT OUTER JOIN objects o ON m.id=o.mid LEFT OUTER JOIN cats c ON o.cid=c.id");
+$cMenu=null;
+$cCat=null;
+$menu="";
+$cArray=array();
+$data="";
+while($tmp=$sql->fetch_array()) {
+    if($tmp['id']!=$cMenu) {
+        if(!empty($tmp['mlink'])) {
+            $link=$tmp['mlink'];
+        }
+        else {
+            $link=strtolower(cleanString($tmp['name']));
+        }
+        $menu.="<li><a href=#$link>$tmp[name]</a></li>\n";
+        unset($link);
+        if(!empty($cCat)) {
+            for($x=0; $x<sizeof($cArray); $x++) {
+                
+            }
+        }
+    }    
+}
 
+print $menu;
+
+function cleanString($string) {
+    $tmp=str_replace("'",'',$string);
+    $tmp=str_replace("`",'',$tmp);
+    $tmp=str_replace(" ",'',$tmp);
+    return $tmp;
+}
 ?>
