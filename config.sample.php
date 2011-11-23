4f41254e0d61c0c7cf6dd2e338610f9fdf65e2dc
Sean Burke <seanb@eshark.net>
2011-11-23 15:54:52 -0500

    Moved config vars to config.php and created sample file

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+$dbHost='seansspace.com'
+$dbUSer='thb'
+$dbPass='g00b3r'
+$dbName='terrariahb'
+
+$baseDir='/srv/www/projects/droid/thb/';
+$templateFile='template.html';
+$indexFile='index.html';
+$imageDir='img/';
+?>
